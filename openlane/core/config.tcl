cd39131d317b247af31207b6abeb9509f9023193
Rodrigo Huerta <rodrigodenia@gmail.com>
2021-11-06 23:54:57 +0100

    Core fixed error timing issues with hold slack margins

--- openlane/core/config.tcl
+++ openlane/core/config.tcl
@@ -40,7 +40,7 @@ set ::env(DESIGN_IS_CORE) 0
 
 set ::env(CLOCK_PORT) "clk"
 set ::env(CLOCK_NET) "clk"
-set ::env(CLOCK_PERIOD) "75"
+set ::env(CLOCK_PERIOD) "50"
 
 set ::env(FP_SIZING) absolute
 set ::env(DIE_AREA) "0 0 1500 1500"
@@ -68,10 +68,18 @@ set ::env(DIODE_INSERTION_STRATEGY) 4
 # If you're going to use multiple power domains, then disable cvc run.
 set ::env(RUN_CVC) 1
 
+set ::env(PL_RESIZER_HOLD_SLACK_MARGIN) "0.3"
+set ::env(GLB_RESIZER_HOLD_SLACK_MARGIN) "0.3"
+# set ::env(GLB_RESIZER_HOLD_MAX_BUFFER_PERCENT) "30"
 # set ::env(GLB_RESIZER_HOLD_MAX_BUFFER_PERCENT) "30"
 # set ::env(PL_RESIZER_HOLD_MAX_BUFFER_PERCENT) "30"
 # set ::env(PL_RESIZER_ALLOW_SETUP_VIOS) 1
 # set ::env(GLB_RESIZER_ALLOW_SETUP_VIOS) 1
 
+# set ::env(CTS_CLK_BUFFER_LIST) "sky130_fd_sc_hd__clkbuf_2"
+# set ::env(SYNTH_STRATEGY) "AREA 2" 
+# default 2
+# set ::env(PL_RESIZER_TIMING_OPTIMIZATIONS) 0 
+# set ::env(GLB_RESIZER_TIMING_OPTIMIZATIONS) 0 
 
 set ::env(ROUTING_CORES) 6
